5cc1cbea6083b6536b9b2749b383cab83d6411f5
unknown <abdulmoiz3140@gmail.com>
2024-09-05 04:42:09 +0500

    Landing Page desing

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
 import Hero from "@/components/hero";
 import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
+import About from "@/components/about";
+import Pricing from "@/components/pricing";
+
 
 
 
@@ -8,6 +12,10 @@ export default function Home() {
     <main className="flex flex-col min-h-dvh">
       <NavBar />
       <Hero />
+      <About/>
+      <Pricing/>
+      <Footer/>
+      
       
       
     </main>
