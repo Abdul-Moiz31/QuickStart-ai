4386c059057b6279953e1e62e87cbd33571adfcd
unknown <abdulmoiz3140@gmail.com>
2024-09-04 20:30:59 +0500

    inital commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/hero";
+import NavBar from "@/components/navbar";
+
+
+
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-dvh">
+      <NavBar />
+      <Hero />
+      
+      
+    </main>
+  );
+}
