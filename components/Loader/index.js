b9bd7c92f5524ea651af8c38289c6d47dce75a9c
Abdul Moiz <abdulmoiz3140@gmail.com>
2024-09-28 17:05:24 +0500

    Done

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -16,7 +16,7 @@ const Loader = () => {
 
         @keyframes spin {
           0% {
-            transform: rotate(0deg); /* Start position */
+            transform: rotate(0deg); 
           }
           100% {
             transform: rotate(360deg); /* End position */
